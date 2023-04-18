9d89c4d3ac6d4a8704df77f3e22afa9364e35439
Leo Lin <leolin93@live.com>
2023-04-17 19:18:11 -0700

    Sorted plugins alphabetically

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -1,37 +1,56 @@
 return {
-    'kyazdani42/nvim-web-devicons', -- Cool icons
-    'nvim-lualine/lualine.nvim', -- Lualine status bar
-    { 'catppuccin/nvim', name = 'catppuccin' }, -- Catppuccin colorscheme
-    'EdenEast/nightfox.nvim', -- Nightfox colorscheme
-    'preservim/nerdcommenter', --NERD Commenter
-    'nvim-treesitter/nvim-treesitter', -- Treesitter
-    'nvim-tree/nvim-tree.lua', -- NERDTree for nvim
-    'nanozuki/tabby.nvim', -- Tabby tab bar
-    'lukas-reineke/indent-blankline.nvim', -- Indent Guides
-    'windwp/nvim-autopairs', -- Nvim autopairs
-    'xiyaowong/nvim-transparent', -- Transparent background
-    'HiPhish/nvim-ts-rainbow2', -- Rainbow parantheses
-    'neovim/nvim-lspconfig', -- Nvim LSP config
-    'lewis6991/gitsigns.nvim', -- Git signs
-    'habamax/vim-godot', -- Godot Engine integration
-    { 'nvim-telescope/telescope.nvim', tag = '0.1.1',
-    dependencies = { 'nvim-lua/plenary.nvim' } }, -- Nvim telescope
-    'sindrets/diffview.nvim', -- Diffview
     { 'akinsho/toggleterm.nvim', version = "*", config = true }, -- Toggleterm
-    'tpope/vim-fugitive', -- Vim Fugitive, so good it should be illegal
-    'neovim/nvim-lspconfig', -- Ready-made lsp configuration
-    'hrsh7th/cmp-nvim-lsp', -- Nvim cmp autocompletion
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-path',
+
+    { 'catppuccin/nvim', name = 'catppuccin' }, -- Catppuccin colorscheme
+
+    'habamax/vim-godot', -- Godot Engine integration
+
+    'hrsh7th/cmp-buffer', -- Nvim cmp autocompletion
     'hrsh7th/cmp-cmdline',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-path',
     'hrsh7th/nvim-cmp',
-    'L3MON4D3/LuaSnip', -- Luasnip for code snippets
-    'saadparwaiz1/cmp_luasnip',
-    'phaazon/hop.nvim', -- Hop for easy motions
-    {
-        "williamboman/mason.nvim", -- Mason for managing LSP server installs
-        build = ":MasonUpdate" -- :MasonUpdate updates registry contents
-    },
-    { 'williamboman/mason-lspconfig.nvim', dependencies = { 'williamboman/mason.nvim' } },
+
+    'kyazdani42/nvim-web-devicons', -- Cool icons
+
+    'lewis6991/gitsigns.nvim', -- Git signs
+
+    'lukas-reineke/indent-blankline.nvim', -- Indent Guides
+
     'mfussenegger/nvim-lint', -- Nvim-lint for linting support
+
+    'nanozuki/tabby.nvim', -- Tabby tab bar
+
+    'neovim/nvim-lspconfig', -- Ready-made lsp configuration
+
+    'nvim-lualine/lualine.nvim', -- Lualine status bar
+
+    { 'nvim-telescope/telescope.nvim', tag = '0.1.1', dependencies = { 'nvim-lua/plenary.nvim' } }, -- Nvim telescope
+
+    'nvim-tree/nvim-tree.lua', -- NERDTree for nvim
+
+    'nvim-treesitter/nvim-treesitter', -- Treesitter
+
+    'phaazon/hop.nvim', -- Hop for easy motions
+
+    'preservim/nerdcommenter', --NERD Commenter
+
+    'saadparwaiz1/cmp_luasnip', -- Luasnip integration into cmp
+
+    'sindrets/diffview.nvim', -- Diffview
+
+    'tpope/vim-fugitive', -- Vim Fugitive, so good it should be illegal
+
+    'windwp/nvim-autopairs', -- Nvim autopairs
+
+    { "williamboman/mason.nvim", build = ":MasonUpdate" }, -- Mason for managing LSP installs
+    { 'williamboman/mason-lspconfig.nvim', dependencies = { 'williamboman/mason.nvim' } },
+
+    'xiyaowong/nvim-transparent', -- Transparent background
+
+    'EdenEast/nightfox.nvim', -- Nightfox colorscheme
+
+    'HiPhish/nvim-ts-rainbow2', -- Rainbow parantheses
+
+    'L3MON4D3/LuaSnip', -- Luasnip for code snippets
 }
