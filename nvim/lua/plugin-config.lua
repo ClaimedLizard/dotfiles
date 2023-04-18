6eabd7a3beddaff1f070318988a8f5dcf4c70db3
ClaimedLizard <leolin93@live.com>
2023-04-17 23:03:47 -0700

    Nvim tree closes on open file

--- nvim/lua/plugin-config.lua
+++ nvim/lua/plugin-config.lua
@@ -139,20 +139,11 @@ require("diffview").setup({
 ------ Config for nvim tree ------
 local gheight = vim.api.nvim_list_uis()[1].height
 require("nvim-tree").setup({
-    view = {
-        float = {
-            enable = true,
-            quit_on_focus_loss = true,
-            open_win_config = {
-                relative = "editor",
-                border = "rounded",
-                width = 30,
-                height = gheight - 5,
-                row = 1,
-                col = 1,
-            }
+    actions = {
+        open_file = {
+            quit_on_open = true,
         }
-    }
+    },
 })
 
 ------ Mason nvim config ------
